d22331a4ce8f673c77ca3c21379f8749d6a5c1bc
kiplah <kiplahvictor27@gmail.com>
2026-06-07 17:37:54 +0300

    Update ESLint and Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ["encrypted-tbn0.gstatic.com","via.placeholder.com"],
       },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
